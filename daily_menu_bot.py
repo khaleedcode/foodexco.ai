772df12276acbc2228046e856e612399f8c80a84
khaleedcode <164218329+khaleedcode@users.noreply.github.com>
2025-07-02 01:39:02 +0800

    Update daily_menu_bot.py
    should be 21:30 instead because code is in 24hr system

--- daily_menu_bot.py
+++ daily_menu_bot.py
@@ -57,7 +57,7 @@ async def menu_tomorrow_command(interaction: discord.Interaction):
 @tasks.loop(hours=24)
 async def daily_menu():
     now = datetime.now(tz)
-    future = now.replace(hour=9, minute=30, second=00, microsecond=0)
+    future = now.replace(hour=21, minute=30, second=00, microsecond=0)
     if now > future:
         future = future + timedelta(days=1)
     await asyncio.sleep((future - now).seconds)
